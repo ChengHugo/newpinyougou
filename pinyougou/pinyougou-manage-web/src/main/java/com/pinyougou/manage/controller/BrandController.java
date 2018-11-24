83b2e4bad8a834a1b917688a967a20b4df4fe074
liaojianbin <meinitcasthavefun@foxmail.com>
2018-11-24 16:24:00 +0800

    品牌修改

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -20,6 +20,24 @@ public class BrandController {
     private BrandService brandService;
 
     /**
+     * 更加主键更新品牌数据到数据库中
+     * @param brand 品牌
+     * @return 操作结果
+     */
+    @PostMapping("/update")
+    public Result update(@RequestBody TbBrand brand){
+        try {
+            brandService.update(brand);
+
+            return Result.ok("更新品牌成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("更新品牌失败");
+    }
+
+
+    /**
      * 根据品牌主键查询品牌
      * @param id 品牌主键
      * @return 品牌
