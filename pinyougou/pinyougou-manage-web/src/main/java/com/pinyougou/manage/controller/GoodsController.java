cda88e5349bdfa869753c54a483d88d600aad1af
liaojianbin <meinitcasthavefun@foxmail.com>
2018-11-27 11:37:07 +0800

    商家审核实现

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/GoodsController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/GoodsController.java
@@ -1,62 +1,52 @@
-package com.pinyougou.shop.controller;
+package com.pinyougou.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.pinyougou.pojo.TbSeller;
-import com.pinyougou.sellergoods.service.SellerService;
+import com.pinyougou.pojo.TbGoods;
+import com.pinyougou.sellergoods.service.GoodsService;
 import com.pinyougou.vo.PageResult;
 import com.pinyougou.vo.Result;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
-@RequestMapping("/seller")
+@RequestMapping("/goods")
 @RestController
-public class SellerController {
+public class GoodsController {
 
     @Reference
-    private SellerService sellerService;
+    private GoodsService goodsService;
 
     @RequestMapping("/findAll")
-    public List<TbSeller> findAll() {
-        return sellerService.findAll();
+    public List<TbGoods> findAll() {
+        return goodsService.findAll();
     }
 
     @GetMapping("/findPage")
     public PageResult findPage(@RequestParam(value = "page", defaultValue = "1")Integer page,
                                @RequestParam(value = "rows", defaultValue = "10")Integer rows) {
-        return sellerService.findPage(page, rows);
+        return goodsService.findPage(page, rows);
     }
 
-    /**
-     * 保存商家信息
-     * @param seller 商家
-     * @return 操作结果
-     */
     @PostMapping("/add")
-    public Result add(@RequestBody TbSeller seller) {
+    public Result add(@RequestBody TbGoods goods) {
         try {
-            //未审核
-            seller.setStatus("0");
-            seller.setCreateTime(new Date());
-
-            sellerService.add(seller);
-            return Result.ok("商家注册成功");
+            goodsService.add(goods);
+            return Result.ok("增加成功");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return Result.fail("商家注册失败");
+        return Result.fail("增加失败");
     }
 
     @GetMapping("/findOne")
-    public TbSeller findOne(String id) {
-        return sellerService.findOne(id);
+    public TbGoods findOne(Long id) {
+        return goodsService.findOne(id);
     }
 
     @PostMapping("/update")
-    public Result update(@RequestBody TbSeller seller) {
+    public Result update(@RequestBody TbGoods goods) {
         try {
-            sellerService.update(seller);
+            goodsService.update(goods);
             return Result.ok("修改成功");
         } catch (Exception e) {
             e.printStackTrace();
@@ -65,9 +55,9 @@ public class SellerController {
     }
 
     @GetMapping("/delete")
-    public Result delete(String[] ids) {
+    public Result delete(Long[] ids) {
         try {
-            sellerService.deleteByIds(ids);
+            goodsService.deleteByIds(ids);
             return Result.ok("删除成功");
         } catch (Exception e) {
             e.printStackTrace();
@@ -77,15 +67,15 @@ public class SellerController {
 
     /**
      * 分页查询列表
-     * @param seller 查询条件
+     * @param goods 查询条件
      * @param page 页号
      * @param rows 每页大小
      * @return
      */
     @PostMapping("/search")
-    public PageResult search(@RequestBody  TbSeller seller, @RequestParam(value = "page", defaultValue = "1")Integer page,
+    public PageResult search(@RequestBody  TbGoods goods, @RequestParam(value = "page", defaultValue = "1")Integer page,
                                @RequestParam(value = "rows", defaultValue = "10")Integer rows) {
-        return sellerService.search(page, rows, seller);
+        return goodsService.search(page, rows, goods);
     }
 
 }
