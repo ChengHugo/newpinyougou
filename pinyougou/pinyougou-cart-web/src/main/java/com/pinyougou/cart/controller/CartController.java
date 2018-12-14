a14d923e13d2d5cda8161be4b82a35aa9a1826e2
liaojianbin <meinitcasthavefun@foxmail.com>
2018-12-14 10:37:27 +0800

    显示登录用户名

---
+++ pinyougou/pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
@@ -0,0 +1,29 @@
+package com.pinyougou.cart.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/cart")
+@RestController
+public class CartController {
+
+    /**
+     * 获取当前登录用户名
+     * @return 用户信息
+     */
+    @GetMapping("/getUsername")
+    public Map<String, Object> getUsername(){
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        //因为配置了可以匿名访问所以如果是匿名访问的时候，返回的用户名为anonymousUser
+        //如果未登录则用户名为：anonymousUser
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        map.put("username", username);
+        return map;
+    }
+}
