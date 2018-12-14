64a19ad7e7bf0eac9bcff7a95a1eab2dbe3afd8b
liaojianbin <meinitcasthavefun@foxmail.com>
2018-12-14 11:08:50 +0800

    未登录-购物车列表加载-后端实现

--- pinyougou/pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
+++ pinyougou/pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
@@ -1,16 +1,62 @@
 package com.pinyougou.cart.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.pinyougou.common.util.CookieUtils;
+import com.pinyougou.vo.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RequestMapping("/cart")
 @RestController
 public class CartController {
+
+    //品优购系统的购物车在cookie中的名称
+    private static final String COOKIE_CART_LIST = "PYG_CART_LIST";
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private HttpServletResponse response;
+
+
+    /**
+     * 登录或者未登录情况下购物车列表的查询
+     * @return 购物车列表
+     */
+    @GetMapping("/findCartList")
+    public List<Cart> findCartList(){
+        try {
+            //因为配置了可以匿名访问所以如果是匿名访问的时候，返回的用户名为anonymousUser
+            //如果未登录则用户名为：anonymousUser
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            if ("anonymousUser".equals(username)) {
+                //未登录；从cookie中获取购物车数据
+                List<Cart> cookieCartList = new ArrayList<>();
+                String cartListJsonStr = CookieUtils.getCookieValue(request, COOKIE_CART_LIST, true);
+                if (!StringUtils.isEmpty(cartListJsonStr)) {
+                   cookieCartList = JSONArray.parseArray(cartListJsonStr, Cart.class);
+                }
+                return cookieCartList;
+            } else {
+                //已登录，从redis中获取购物车数据
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
 
     /**
      * 获取当前登录用户名
--- pinyougou/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
+++ pinyougou/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
@@ -1,8 +1,18 @@
 app.controller("cartController", function ($scope, cartService) {
 
+    //获取用户信息
     $scope.getUsername = function () {
         cartService.getUsername().success(function (response) {
             $scope.username = response.username;
+
+        });
+
+    };
+
+    //获取购物车列表数据
+    $scope.findCartList = function () {
+        cartService.findCartList().success(function (response) {
+            $scope.cartList = response;
 
         });
 
