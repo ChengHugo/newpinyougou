ca526e888e150a093267bc5b1fc74f7cd0ba3ea5
liaojianbin <meinitcasthavefun@foxmail.com>
2018-12-14 12:29:46 +0800

    购物车列表计算总价&总数量

--- pinyougou/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
+++ pinyougou/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
@@ -14,6 +14,8 @@ app.controller("cartController", function ($scope, cartService) {
         cartService.findCartList().success(function (response) {
             $scope.cartList = response;
 
+            //计算总数量和价格
+            $scope.totalValue = cartService.subTotalValue(response);
         });
 
     };
